9cd11996571e36a9e6eea5edd9bc119519ce9ce9
Sameer Kumar <sameer18051998@gmail.com>
2017-01-06 03:32:53 +0530

    This project scrapes the name and price of all the cars of almost any brand from CarDekho.com . Also various conditions can be imposed on it, such as minimum cost price or scraping of any other data from CarDekho.

---
+++ combined.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def retrieval(url):
+    page = requests.get(url)
+    souped = BeautifulSoup(page.text)
+    tag = souped.find_all(class_='carbrandpage')[0].find_all(class_='clearfix')
+    cars = {}
+
+    for tags in tag:
+        name = tags.find_all('h3')[0].text.strip()
+        price = tags.find_all(class_='price')[0].text
+
+        if name in cars.keys():
+            pass
+        else:
+            print "Name of the car is %s and the cost is %s" %(name, price)
+
+def main():
+
+    input = int(raw_input("""Which brand ? \n Press 0 for Hyundai \n Press 1 for Maruti
+                           \n Press 2 for Skoda \n Press 3 for BMW \n Press 4 for Tata
+                          \n Press 5 for Honda"""))
+#    url = ["https://www.cardekho.com/cars/Hyundai","https://www.cardekho.com/cars/Maruti",
+#          "https://www.cardekho.com/cars/Skoda","https://www.cardekho.com/cars/bmw",
+#           "https://www.cardekho.com/cars/tata","https://www.cardekho.com/cars/honda"]
+    base = "https://www.cardekho.com/cars/"
+    url = ["hyundai","maruti","skoda","bmw","tata","honda"]
+
+    if input == 0:
+        retrieval(base+url[input])
+    if input == 1:
+        retrieval(base+url[input])
+    if input == 2:
+        retrieval(base+url[input])
+    if input == 3:
+        retrieval(base+url[input])
+    if input == 4:
+        retrieval(base+url[input])
+    if input == 5:
+        retrieval(base+url[input])
+
+
+
+main()
